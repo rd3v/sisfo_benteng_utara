4a877084016e7056613adf963770280c9725ce00
rd3v <blackscreen405@yahoo.co.id>
2020-02-17 06:18:52 +0800

    add view bantuan

---
+++ desa/themes/cosmos/partials/bantuan.php
@@ -0,0 +1,45 @@
+<?php  if(!defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+<h2 class="judul-artikel">
+	Daftar Warga Penerima Bantuan Sosial di Kelurahan  <?=$desa["nama_desa"]?>
+</h2>
+<div class="mt-3 arsip stat">
+	<div class="table-responsive">
+		<?php if(count($bantuan_sosial)>0): ?>
+			<table class="table table-striped table-bordered">
+				<thead>
+					<tr>
+						<td width="3%"><b>No.</b></td>
+						<td width="20%"><b>Nama</b></td>
+						<td><b>NIK</b></td>
+						<td><b>Program</b></td>
+						<td width="20%"><b>Masa Berlaku</b></td>
+					</tr>
+				</thead>
+				<tbody>
+				<?php foreach($bantuan_sosial AS $data): ?>
+					<tr>
+						<td style="text-align:center;">
+							<?= $data['no'] ?>
+						</td>
+						<td>
+							<?= $data["nama"] ?>
+						</td>
+						<td>
+							<?= $data["nik"] ?>
+						</td>
+						<td>
+							<?= $data['program'] ?>
+						</td>
+						<td style="text-align:center;">
+							<?= tgl_indo($data["mulai"])." - ".tgl_indo($data["akhir"]) ?>
+						</td>
+					</tr>
+				<?php endforeach; ?>
+				</tbody>
+			</table>
+		<?php else: ?>
+			Belum ada daftar penerima bantuan.
+		<?php endif; ?>
+	</div>
+</div>
