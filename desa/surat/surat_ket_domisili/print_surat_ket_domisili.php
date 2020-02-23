c8b16cb7bde1b3c6509602f26c1bebc310150963
rd3v <blackscreen405@yahoo.co.id>
2020-02-24 06:11:23 +0800

    surat keterangan domisili

--- desa/surat/surat_ket_domisili/print_surat_ket_domisili.php
+++ desa/surat/surat_ket_domisili/print_surat_ket_domisili.php
@@ -1,100 +1,135 @@
-<?php $this->load->view('print/headjs.php');?>
-
-<body>
-<div id="content" class="container_10 clearfix">
-<div id="content-main" class="grid_7">
-
-<link href="<?php echo base_url()?>assets/css/surat.css" rel="stylesheet" type="text/css" />
-<div>
-<table width="100%">
-
-<tr> <img src="<?php echo LogoDesa($desa['logo']);?>" alt="" class="logo"></tr>
-
-<div class="header">
-<h4 class="kop">PEMERINTAH KABUPATEN <?php echo strtoupper($desa['nama_kabupaten'])?> </h4>
-<h4 class="kop">KECAMATAN <?php echo strtoupper($desa['nama_kecamatan'])?> </h4>
-<h4 class="kop">DESA <?php echo strtoupper($desa['nama_desa'])?></h4>
-<h5 class="kop2"><?php echo $desa['alamat_kantor']?> Email : <?php echo ($desa['email_desa'])?> Kode Pos : <?php echo ($desa['kode_pos'])?></h5>
-<div style="text-align: center;">
-<hr /></div></div>
-
-
-</table>
-<table width="100%">
-<div align="center"><u><h5 class="ko2">SURAT KETERANGAN DOMISILI</h5></u></div>
-<div align="center"><h5  class="">Nomor : 470/ <?php echo $input['nomor']?>/PEM/<?php echo date("Y")?></h4></div>
-</table>
-
-<div class="clear"></div>
-<table width="100%">
-<td  class="indentasi" colspan="3">Yang bertanda tangan dibawah ini Kepala Desa <?php echo $desa['nama_desa']?> Kecamatan <?php echo $desa['nama_kecamatan']?> Kabupaten <?php echo $desa['nama_kabupaten']?> Provinsi <?php echo $desa['nama_propinsi']?> menerangkan dengan sebenarnya bahwa :  </td></tr>
-</table>
-</table><div id="isi3">
-<table width="100%">
-<tr><td width="23%">Nama Lengkap</td><td width="1%">:</td><td width="60%"><b><?php echo $data['nama']?></td></tr>
-<tr><td width="23%">NIK/ No KTP</td><td width="1%">:</td><td width="60%"><?php echo $data['nik']?></td></tr>
-<tr><td>Tempat dan Tgl. Lahir </td><td>:</td><td><?php echo ($data['tempatlahir'])?>, <?php echo tgl_indo($data['tanggallahir'])?> </td></tr>
-<tr><td>Jenis Kelamin</td><td>:</td><td><?php echo $data['sex']?></td></tr>
-<tr><td>Agama</td><td>:</td><td><?php echo $data['agama']?></td></tr>
-<tr><td>Status</td><td>:</td><td><?php echo $data['status_kawin']?></td></tr>
-<tr><td>Pendidikan</td><td>:</td><td><?php echo $data['pendidikan']?></td></tr>
-<tr><td>Pekerjaan</td><td>:</td><td><?php echo $data['pekerjaan']?></td></tr>
-<tr><td>Kewarganegaraan </td><td>:</td><td><?php echo $data['warganegara']?></td></tr>
-<tr><td>Alamat/ Tempat Tinggal</td><td>:</td><td> <?php echo ucwords(strtolower($data['alamat']))?> RT. <?php echo $data['rt']?> RW. <?php echo $data['rw']?> Dusun <?php echo $data['dusun']?> Desa <?php echo $desa['nama_desa']?> Kecamatan <?php echo $desa['nama_kecamatan']?> Kabupaten <?php echo $desa['nama_kabupaten']?></td></tr>
-</table>
-<table width="100%">
-<td  class="indentasi" colspan="3">Orang tersebut diatas adalah benar-benar warga kami yang bertempat tinggal di <?php echo ucwords(strtolower($data['alamat']))?> RT. <?php echo $data['rt']?> RW. <?php echo $data['rw']?> Dusun <?php echo $data['dusun']?> Desa <?php echo $desa['nama_desa']?> Kecamatan <?php echo $desa['nama_kecamatan']?> Kabupaten <?php echo $desa['nama_kabupaten']?> dan tercatat dengan
-No. KK : <?php echo $data['no_kk']?> Kepala Keluarga : <?php echo $data['kepala_kk']?>.</td>
-</table>
-<table width="100%">
-<td  class="indentasi" colspan="3">Surat Keterangan ini dibuat untuk Keperluan : <b><?php echo $input['keperluan']?></td></tr>
-<tr></tr>
-</table>
-<table width="100%">
-
-<td  class="indentasi" colspan="3">Demikian Surat Keterangan ini dibuat dengan sebenarnya untuk dapat dipergunakan sebagaimana mestinya.</td>
-</table>
-</tr></td>
-<tr></tr>
-<td colspan="3">
-<tr></tr>
-<br></br>
-<tr></tr>
-<tr></tr>
-<tr></tr>
-</table></div>
-<table width="100%">
-<tr></tr>
-<tr><td></td><td width="55%"></td><td align="center"><?php echo $desa['nama_desa']?>, <?php echo $tanggal_sekarang?></td></tr>
-<tr><td></td><td width="55%"></td><td align="center"><?php echo ($input['atas_nama'])?></td></tr>
-<tr><td></td><td width="55%"></td><td align="center"><?php echo $input['jabatan']?>,</td></tr>
-</table></div>
-<table width="100%">
-<tr></tr>
-<tr></tr>
-<tr></tr>
-<tr></tr>
-<tr></tr>
-<tr></tr>
-<tr></tr>
-<tr></tr>
-<tr></tr>
-<tr></tr>
-<tr></tr>
-<tr></tr>
-<tr></tr>
-<tr></tr>
-<tr></tr>
-<tr></tr>
-<tr></tr>
-<tr></tr>
-<tr></tr>
-<tr></tr>
-<tr><td></td><td width="55%"></td><td align="center"><b><u><?php echo $input['pamong']?> </u></td></tr>
-<tr><td></td><td width="55%"></td><td align="center"><?php echo ($input['pamong_nip'])?></td></tr>
-</table>  </div></div>
-<div id="aside">
-</div>
-</div>
-</body>
+<html>
+	<?php $this->load->view('print/headjs.php');?>
+	<body>
+		<div id="content" class="container_12 clearfix">
+			<div id="content-main" class="grid_7">
+				<link href="<?= base_url()?>assets/css/surat.css" rel="stylesheet" type="text/css" />
+				<div>
+					<table width="100%">
+						<tr> 
+							<td align="center">		
+							<img src="<?= LogoDesa($desa['logo']);?>" alt=""  width="100" height="80">
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<div class="header">
+									<h4 class="kop3">PEMERINTAH KABUPATEN <?= strtoupper(unpenetration($desa['nama_kabupaten']))?> </h4>
+									<h4 class="kop3">KECAMATAN <?= strtoupper(unpenetration($desa['nama_kecamatan']))?> </h4>
+									<h4 class="kop3">DESA <?= strtoupper(unpenetration($desa['nama_desa']))?></h4>
+									<h5 class="kop2"><?= ($desa['alamat_kantor'])?> </h5>
+									<div style="text-align: center;"><hr></div>
+								</div>								
+							</td>
+						</tr>
+						<tr>
+							<td>
+						<div align="center"><u><h4 class="kop3">SURAT KETERANGAN DOMISILI</h4></u></div>
+						<div align="center"><h4 class="kop3">Nomor : <?= $input['nomor']?></h4></div>								
+							</td>
+						</tr>
+					</table>
+					<table width="100%">
+						<tr>
+							<td colspan="3"><p>Yang bertanda tangan dibawah ini : </p></td>
+						</tr>
+						<tr>
+							<td width="10%">a. Nama</td>
+							<td width="2%">:</td>
+							<td><?= unpenetration($input['pamong']) ?></td>
+						</tr>
+						<tr>
+							<td width="10%">b. Jabatan</td>
+							<td width="2%">:</td>
+							<td><?= unpenetration($input['jabatan']) ?></td>
+						</tr>
+					</table>
+					<div id="isi3">
+						<tr></tr>
+						<tr></tr>
+						<tr></tr>
+						<tr><td>
+							<table width="100%">
+								<tr><td width="20%">1. Nama Lengkap</td><td width="2%">:</td><td width="64%"><?= unpenetration($data['nama'])?></td></tr>
+								<tr><td>2. NIK / Nomor KTP</td><td>:</td><td><?= $data['nik']?></td></tr>
+								<tr><td>3. No. KK</td><td>:</td><td><?= $data['no_kk']?></td></tr>
+								<tr><td>4. Tempat dan Tgl. Lahir </td><td>:</td><td><?= ($data['tempatlahir'])?>, <?= tgl_indo($data['tanggallahir'])?> </td></tr>
+								<tr><td>5. Jenis Kelamin</td><td>:</td><td><?= $data['sex']?></td></tr>
+								<tr><td>6. Alamat / Tempat Tinggal</td><td>:</td><td>RT. <?= $data['rt']?>, RW. <?= $data['rw']?>, Dusun <?= ununderscore(unpenetration($data['dusun']))?>, Desa <?= unpenetration($desa['nama_desa'])?>, Kec. <?= unpenetration($desa['nama_kecamatan'])?>, Kab. <?= unpenetration($desa['nama_kabupaten'])?></td></tr>
+								<tr><td>7. Agama</td><td>:</td><td><?= $data['agama']?></td></tr>
+								<tr><td>8. Status</td><td>:</td><td><?= $data['status_kawin']?></td></tr>
+								<tr><td>9. Pendidikan</td><td>:</td><td><?= $data['pendidikan']?></td></tr>
+								<tr><td>10. Pekerjaan</td><td>:</td><td><?= $data['pekerjaan']?></td></tr>
+								<tr><td>11. Kewarganegaraan </td><td>:</td><td><?= $data['warganegara']?></td></tr>
+								<tr>
+									<td colspan="3"><p></p></td>
+								</tr>
+								<tr>
+									<td><p><b>&nbsp;&nbsp;&nbsp;&nbsp;Maksud</b></p></td>
+									<td><p>:</p></td>
+									<td>
+										<p>
+											Bahwa yang tersebut di atas Benar Berdomisili di Jalan <?= $data['alamat'] ?> Lingkungan <?= $input['lingkungan'] ?> Kelurahan Benteng Utara Kecamatan Benteng Kabupaten Kepulauan Selayar.
+										</p>
+									</td>
+								</tr>
+								
+							</table>
+							<table width="100%" style="margin: 0">
+								<tr><td  class="indentasi">Demikian Surat Keterangan ini dibuat dan dan diberikan kepada yang bersangkutan untuk digunakan sebagaimana mestinya. </td></tr>
+								<tr></tr>
+								<tr></tr>
+								<tr></tr>
+								<tr></tr>
+								<tr></tr>
+								<tr></tr>
+							</table>
+						</div>
+						<table width="100%">
+							<tr></tr>
+							<tr></tr>
+							<tr></tr>
+							<tr></tr>
+							<tr></tr>
+							<tr><td></td><td width="30%"></td><td  align="center"><?= unpenetration($desa['nama_desa'])?>, <?= $tanggal_sekarang?></td></tr>
+							<tr><td></td><td width="30%"></td><td align="center"><?= unpenetration($input['jabatan'])?> <?= unpenetration($desa['nama_desa'])?></td></tr>
+							<tr></tr>
+							<tr></tr>
+							<tr></tr>
+							<tr></tr>
+							<tr></tr>
+							<tr></tr>
+							<tr></tr>
+							<tr></tr>
+							<tr></tr>
+							<tr></tr>
+							<tr></tr>
+							<tr></tr>
+							<tr></tr>
+							<tr></tr>
+							<tr></tr>
+							<tr></tr>
+							<tr></tr>
+							<tr></tr>
+							<tr></tr>
+							<tr></tr>
+							<tr></tr>
+							<tr></tr>
+							<tr></tr>
+							<tr></tr>
+							<tr></tr>
+							<tr></tr>
+							<tr></tr>
+							<tr></tr>
+							<tr></tr>
+							<tr><td> <td></td><td align="center">( <?= unpenetration($input['pamong'])?> )</td></tr>
+						</table>
+					</div>
+				</div>
+			</div>
+			<div id="aside"></div>
+			<div id="footer" class="container_12"></div>
+		</div>
+	</body>
 </html>
+
