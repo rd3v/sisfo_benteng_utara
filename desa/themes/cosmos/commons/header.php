d6c0bfe68d223b111858f41d05f751d6540997cf
rd3v <blackscreen405@yahoo.co.id>
2020-02-26 15:24:06 +0800

    update banner

--- desa/themes/cosmos/commons/header.php
+++ desa/themes/cosmos/commons/header.php
@@ -3,29 +3,9 @@
 <header>
 
 	<?php $this->load->view($folder_themes . '/partials/marquee'); ?>
-	<div class="identitas-desa">
-		<div class="container">
-			<a href="<?= site_url('first'); ?>" class="brand">
-				<div class="logo">
-						<img src="<?= LogoDesa($desa['logo']) ?>" alt="<?= ucfirst($this->setting->sebutan_desa).' '.ucwords($desa['nama_desa']) ?>" class="img-fluid">
-				</div>
-				<div class="detail">
-					<h1 class="h1">
-						<span><?= strtoupper($this->setting->sebutan_desa) ?></span>
-						<span><?= strtoupper($desa['nama_desa']); ?></span>
-					</h1>
-					<div class="ket">
-						<span>
-						<?= ucfirst($this->setting->sebutan_kecamatan_singkat) ?> 
-						<?= ucwords($desa['nama_kecamatan']) ?>
-						<?= ucfirst($this->setting->sebutan_kabupaten_singkat) ?>
-						<?= ucwords($desa['nama_kabupaten']) ?>
-						</span>
-						<?php  ?>
-					</div>
-				</div>
-
-			</a>
-		</div>
+	<div class="identitas-desa" style="padding:0">
+		<a href="<?= site_url('first'); ?>" class="brand">
+			<img src="<?= base_url('assets/images/banner.png') ?>" width="100%" alt="banner">
+		</a>
 	</div>
 </header>
